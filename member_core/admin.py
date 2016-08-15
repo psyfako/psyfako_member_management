556eb806f5ad6d4b358eb1a7db556d60ec79bd66
WU <git@segmentum.solar>
2016-08-15 23:45:57 +0200

    basic member models

--- member_core/admin.py
+++ member_core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(Member)
+admin.site.register(Fachschaft)
+admin.site.register(Event)
+admin.site.register(Allergy)
--- member_core/models.py
+++ member_core/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class Fachschaft(models.Model):
+    university_name = models.CharField(max_length=240, blank=True)
+    university_type = models.CharField(max_length=3, blank=True)
+    city = models.CharField(max_length=240, blank=True)
+    mail = models.EmailField(max_length=254)
+    
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # extendet user stats
+    address_street       = models.CharField(max_length=240, blank=True)
+    address_streetnumber = models.IntegerField()
+    address_city         = models.CharField(max_length=240, blank=True)
+    address_citycode     = models.IntegerField()
+    
+    mobilephone = PhoneNumberField(blank=True)
+    
+    mail = models.EmailField(max_length=254)
+    
+    #Allergy tagging field comes here
+    # allergys = 
+    
+    fachschaft = models.ForeignKey('Fachschaft', on_delete=models.CASCADE,)
+    
+    is_konrat   = models.BooleanField(default=False)
+    is_vorstand = models.BooleanField(default=False)
+    
+
+class Event(models.Model):
+    name      = models.CharField(max_length=240, blank=True)
+    iteration = models.IntegerField()
+    location  = models.CharField(max_length=240, blank=True)
+
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=240, blank=True)
+    
+
+
+    
