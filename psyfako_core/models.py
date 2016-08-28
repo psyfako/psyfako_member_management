e755df697dcb0f6c80c8456402d8213ee4cf4009
WU <git@segmentum.solar>
2016-08-28 12:49:26 +0200

    rename; model event extention

--- psyfako_core/models.py
+++ psyfako_core/models.py
@@ -26,7 +26,6 @@ class Member(models.Model):
     
     #Allergy tagging field comes here
     # allergys = 
-    
     fachschaft = models.ForeignKey('Fachschaft', on_delete=models.CASCADE,)
     
     is_konrat   = models.BooleanField(default=False)
@@ -34,9 +33,12 @@ class Member(models.Model):
     
 
 class Event(models.Model):
-    name      = models.CharField(max_length=240, blank=True)
-    iteration = models.IntegerField()
-    location  = models.CharField(max_length=240, blank=True)
+    name       = models.CharField(max_length=240, blank=True)
+    name_short = models.CharField(max_length=10, blank=True)
+    iteration  = models.IntegerField()
+    location   = models.CharField(max_length=240, blank=True)
+    date_start = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True)
+    date_end   = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True)
 
 
 class Allergy(models.Model):
