48531ad472e8d0b74fb1fd383a5be21b1f79c727
WU <git@segmentum.solar>
2016-11-14 22:16:01 +0100

    django-configrurations implemented

--- django_root/settings.py
+++ django_root/settings.py
@@ -16,121 +16,140 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3i0c3!klxc+7+ci%u-&ql^^bu-4qc^on$vdch6+d1r7f=!(uk$'
+from configurations import Configuration
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'tagulous',
-    'psyfako_core',
-    'timetable',
-]
-
-MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'django_root.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'django_templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+class Common(Configuration):
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+    
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = '3i0c3!klxc+7+ci%u-&ql^^bu-4qc^on$vdch6+d1r7f=!(uk$'
+    
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    
+    ALLOWED_HOSTS = []
+    
+    
+    # Application definition
+    
+    INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'tagulous',
+        'psyfako_core',
+        'timetable',
+    ]
+    
+    MIDDLEWARE_CLASSES = [
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+    
+    ROOT_URLCONF = 'django_root.urls'
+    
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(BASE_DIR, 'django_templates')],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
         },
-    },
-]
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "django_static"),
-]
-
-WSGI_APPLICATION = 'django_root.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    ]
+    
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "django_static"),
+    ]
+    
+    WSGI_APPLICATION = 'django_root.wsgi.application'
+    
+    
+    # Database
+    # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
-
-# Tagulous Serialization
-SERIALIZATION_MODULES = {
-    'xml':    'tagulous.serializers.xml_serializer',
-    'json':   'tagulous.serializers.json',
-    'python': 'tagulous.serializers.python',
-    'yaml':   'tagulous.serializers.pyyaml',
-}
+    
+    
+    # Password validation
+    # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+    
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+    
+    
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.9/topics/i18n/
+    
+    LANGUAGE_CODE = 'en-us'
+    
+    TIME_ZONE = 'UTC'
+    
+    USE_I18N = True
+    
+    USE_L10N = True
+    
+    USE_TZ = True
+    
+    
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.9/howto/static-files/
+    
+    STATIC_URL = '/static/'
+    
+    # Tagulous Serialization
+    SERIALIZATION_MODULES = {
+        'xml':    'tagulous.serializers.xml_serializer',
+        'json':   'tagulous.serializers.json',
+        'python': 'tagulous.serializers.python',
+        'yaml':   'tagulous.serializers.pyyaml',
+    }
+    
+class dev(Common):
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = '3i0c3!klxc+7+ci%u-&ql^^bu-4qc^on$vdch6+d1r7f=!(uk$'
+    
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    
+    
+class stable(Common):
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = '3i0c3!klxc+7+ci%u-&ql^^bu-4qc^on$vdch6+d1r7f=!(uk$'
+    
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = False
--- django_root/wsgi.py
+++ django_root/wsgi.py
@@ -12,5 +12,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "psyfako_member_management.settings")
+os.environ.setdefault('DJANGO_CONFIGURATION', 'dev')
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+
