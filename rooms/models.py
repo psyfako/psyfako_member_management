4c3cf4192fb86ac08d584981720273e2d166d5d6
MichiMolle <michael@moeller-schwandorf.de>
2016-11-24 06:28:28 +0100

    timetablemitadd

--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,10 @@ class Building(models.Model):
     name = models.CharField(max_length=200)
     short_name = models.CharField(max_length=5)
     adress = models.CharField(max_length=200)
+    lat = models.CharField(max_length=200, blank=True)
+    lng = models.CharField(max_length=200, blank=True)
+    
+    
     
     def __str__(self):
         return self.name
@@ -13,7 +17,7 @@ class Room(models.Model):
     name = models.CharField(max_length=200)
     floor = models.CharField(max_length=2)
     nr = models.CharField(max_length=4)
-    capacity = models.IntegerField(max_length=5)
+    capacity = models.IntegerField()
     
     def __str__(self):
         return self.name + " (" +self.building.short_name +"/" + self.floor + "." + self.nr +")" + " - " + str(self.capacity) +" Personen" 
